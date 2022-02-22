3cac2df6e60c609ce25fd7f3a2b85fa90f545df5
Elizabeth Liu <esl1350@gmail.com>
2022-02-22 01:05:12 -0500

    add project files

---
+++ images/cow.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=30x30 cow cow.png 
+ * Time-stamp: Wednesday 11/10/2021, 03:34:33
+ * 
+ * Image Information
+ * -----------------
+ * cow.png 30@30
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef COW_H
+#define COW_H
+
+extern const unsigned short cow[900];
+#define COW_SIZE 1800
+#define COW_LENGTH 900
+#define COW_WIDTH 30
+#define COW_HEIGHT 30
+
+#endif
+
